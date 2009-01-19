f4b342a9e159ff61e6f2da14793330d7b9bfe64a
chris.lukassen <chris.lukassen@199b1064-2552-0410-b72a-c7bb3591c561>
2009-01-19 14:11:58 +0000

    this is release candidate 2

---
+++ IVEF_0_1_0_RC2/ivef-objc/Classes/IVEFMSG_LoginRequest.m
@@ -0,0 +1,76 @@
+
+#import "IVEFMSG_LoginRequest.h"
+
+
+@implementation IVEFMSG_LoginRequest
+
+- (id) init {
+    self = [super init];
+    if (self != nil) {
+        m_dateFormatter = [[NSDateFormatter alloc] init];
+        [m_dateFormatter setDateFormat:@"yyyy-MM-dd'T'HH:mm:ss.SSS"];
+    }
+    return self;
+}
+
+-(void) setHeader:(IVEFHeader *) val {
+
+    [m_header release];
+    m_header = val;
+    [m_header retain];
+}
+
+- (IVEFHeader *) getHeader {
+
+    return m_header;
+}
+
+-(void) setBody:(IVEFBody *) val {
+
+    [m_body release];
+    m_body = val;
+    [m_body retain];
+}
+
+- (IVEFBody *) getBody {
+
+    return m_body;
+}
+
+-(void) setAttributes:(NSDictionary *)attributeDict {
+
+        for (NSString *key in attributeDict) {
+            if ([key isEqualToString: @"Header"]) {
+                IVEFHeader * val = [attributeDict objectForKey: key];
+                [self setHeader: val];
+            }
+            else if ([key isEqualToString:@"Body"]) {
+                IVEFBody * val = [attributeDict objectForKey: key];
+                [self setBody: val];
+            }
+        }
+}
+
+-(NSString *) XML {
+
+    NSMutableString *xml = [NSMutableString stringWithString:@"<MSG_LoginRequest"];
+    [xml appendString:@">\n"];
+    [xml appendString: [m_header XML] ];
+    [xml appendString: [m_body XML] ];
+    [xml appendString: @"</MSG_LoginRequest>\n"];
+    return xml;
+}
+
+-(NSString *) stringValueWithLead: (NSString *) lead {
+
+    NSMutableString *str = [[[NSMutableString alloc] init] autorelease];
+    [str setString: [lead stringByAppendingString:@"MSG_LoginRequest\n"]];
+    [str appendString: [m_header stringValueWithLead: [lead stringByAppendingString: @"    "]] ];
+    [str appendString: [m_body stringValueWithLead: [lead stringByAppendingString: @"    "]] ];
+    return str;
+}
+
+
+@end
+
+
---
+++ IVEF_0_1_0_RC2/ivef-qt/IVEFMSG_Logout.cpp
@@ -0,0 +1,58 @@
+
+#include "IVEFMSG_Logout.h"
+
+MSG_Logout::MSG_Logout() {
+
+}
+
+MSG_Logout::MSG_Logout(const MSG_Logout &val) : QObject() {
+
+    m_header = val.m_header;
+    m_body = val.m_body;
+}
+
+MSG_Logout & MSG_Logout::operator=(const MSG_Logout &val) {
+
+    m_header = val.m_header;
+    m_body = val.m_body;
+    return *this;
+}
+
+void MSG_Logout::setHeader(Header val) {
+
+    m_header = val;
+}
+
+Header MSG_Logout::getHeader() const {
+
+    return m_header;
+}
+
+void MSG_Logout::setBody(Body val) {
+
+    m_body = val;
+}
+
+Body MSG_Logout::getBody() const {
+
+    return m_body;
+}
+
+QString MSG_Logout::toXML() {
+
+    QString xml = "<MSG_Logout";
+    xml.append(">\n");
+    xml.append( m_header.toXML() );
+    xml.append( m_body.toXML() );
+    xml.append( "</MSG_Logout>\n");
+    return xml;
+}
+
+QString MSG_Logout::toString(QString lead) {
+
+    QString str = lead + "MSG_Logout\n";
+    str.append( m_header.toString(lead + "    ") );
+    str.append( m_body.toString(lead + "    ") );
+    return str;
+}
+
