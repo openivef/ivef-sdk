579b1c70f522d9990595fcf6c4792f7165179a5d
chris.lukassen <chris.lukassen@199b1064-2552-0410-b72a-c7bb3591c561>
2010-04-12 19:07:11 +0000

    updated generic test scenario for qt

--- ivef-lib/branches/IVEF_0_1_RELEASE/test-cases/run_all_tests.sh
+++ ivef-lib/branches/IVEF_0_1_RELEASE/test-cases/run_all_tests.sh
@@ -2,22 +2,46 @@ DATADIR=data
 TMPDIR=tmp
 COMMAND='../build/Debug/testobjc'
 
+
+if [ "$1" == "qt" ]
+then
+    export DYLD_LIBRARY_PATH="../ivef-qt/bin"
+    export LD_LIBRARY_PATH="../ivef-qt/bin"
+    COMMAND='../test-qt/bin/testqt'
+elif [ "$1" == "objc" ]
+then
+    COMMAND='../test-objc/build/Debug/testobjc'
+elif [ "$1" == "java" ]
+then
+    COMMAND='java -classpath ../test-java/target/testjava.jar:../ivef-java/ivef/ivef.jar testjava/testjava'
+elif [ "$1" == "all" ]
+then
+    # recursive call of ourselfs
+    $0 qt
+    $0 obj-c
+    $0 java
+    exit 0
+else
+    echo "syntax $0 [qt | objc | java | all]"
+    exit 1
+fi
+
 mkdir -p $TMPDIR
 rm -Rf $TMPDIR/*
 
-for INPUTFILE in `ls ${DATADIR}/*.in` 
+for INPUTFILE in `ls ${DATADIR}/*.xml.in ${DATADIR}/*.xml.$1.in` 
 do
-   TESTCASE=`echo $INPUTFILE | cut -d - -f 1`
-   TESTNAME=`echo $INPUTFILE | cut -d . -f 3`
-   OUTNAME=`echo $INPUTFILE | cut -d . -f -2`.out
+   STCASE=`echo $INPUTFILE | cut -d - -f 1`
+   TESTNAME=`echo $INPUTFILE | cut -d . -f 1`
+   OUTNAME=`echo $INPUTFILE | cut -d . -f -2 | cut -d / -f 2-`.out
 
    echo -n running test $TESTCASE $TESTNAME ......
 
-   cat ${INPUTFILE} | $COMMAND > ${TMPDIR}/$OUTNAME &2>/dev/null
+   cat ${INPUTFILE} | $COMMAND > ${TMPDIR}/$OUTNAME 
 
    if [ ! -f data/$OUTNAME ]
    then
-      echo ERROR i have no reference for test
+      echo ERROR i have no reference for test: data/$OUTNAME
    else
       if [ "`diff -b data/$OUTNAME ${TMPDIR}/$OUTNAME`" == "" ] 
       then 
@@ -32,5 +56,7 @@ do
           exit 1
       fi
    fi
- 
 done 
+
+# all test ok, clean up
+rm -Rf $TMPDIR
