b569a86d5bbafc8ced09451fa501fee26eff0585
chris.lukassen@gmail.com <chris.lukassen@gmail.com@199b1064-2552-0410-b72a-c7bb3591c561>
2010-05-18 12:30:59 +0000

    removed release candidate

--- ivef-lib/tags/IVEF_0_1_7RC1/test-qt/localize.sh
+++
@@ -1,5 +0,0 @@
-otool -L bin/testqt
-PWD=`pwd`
-TARGET=`dirname $PWD`
-install_name_tool -change /ivef.framework/Versions/1/ivef $TARGET/ivef-qt/bin/ivef.framework/Versions/1/ivef bin/testqt
-otool -L bin/testqt
